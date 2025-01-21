5c4a86bae12177c5d0c19762eac939d328f3f5d0
Dimaso <Dimaso.05@mail.ru>
2025-01-21 10:26:09 +0300

    fix xaml

---
+++ T2JuniorMobile/Pages/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace T2JuniorMobile
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
