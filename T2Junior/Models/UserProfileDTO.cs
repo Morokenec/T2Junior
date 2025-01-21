9c731f23054c8ed08e42833fe588bef3f301e943
Matvey-Laptop-ASUS <matveyabramov01@gamil.com>
2025-01-21 12:11:58 +0300

    add UserProfileDTO & UserController

---
+++ T2Junior/Models/UserProfileDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace T2Junior.Models
+{
+    public class UserProfileDTO
+    {
+        public int IdUser { get; set; }
+
+        public string RoleName { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string? Patronymic { get; set; }
+
+        public string? Organization { get; set; }
+
+        public string? Post { get; set; }
+
+        public int? Age { get; set; }
+
+        public sbyte Sex { get; set; }
+
+        public int AccumulatedPoints { get; set; }
+    }
+}
