d2654115a8a5fe767bfcad8936e32e5eed1141c1
Dimaso <Dimaso.05@mail.ru>
2025-01-23 15:42:31 +0300

    Add ConfimPage

--- T2JuniorMobile/ViewModel/RegisterViewModel.cs
+++ T2JuniorMobile/ViewModel/RegisterViewModel.cs
@@ -9,12 +9,41 @@ namespace T2JuniorMobile.ViewModel
 {
     public partial class RegisterViewModel : BaseViewModel
     {
+        private string _fullName;
+        private string _email;
+        private string _numberPhone;
+        private DateTime _date;
+
+        public ICommand NavigateConfimCommand { get; }
+
+        public string Email
+        {
+            get => _email;
+            set => SetProperty(ref _email, value);
+        }
+
+        public string NumberPhone
+        {
+            get => _numberPhone;
+            set => SetProperty(ref _numberPhone, value);
+        }
+
+        public DateTime Date
+        {
+            get => _date;
+            set => SetProperty(ref _date, value);
+        }
+
+        public string FullName
+        {
+            get => _fullName;
+            set => SetProperty(ref _fullName, value);
+        }
+
         public RegisterViewModel()
         {
             NavigateConfimCommand = new Command(async () => await NavigateConfimCommandAsync());
         }
-
-        public ICommand NavigateConfimCommand { get; }
 
         private async Task NavigateConfimCommandAsync()
         {
